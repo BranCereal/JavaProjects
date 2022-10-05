840365605ef1ea6492f09fe3b4efacb841a89dc2
Branden Mckay <65003340+BranCereal@users.noreply.github.com>
2022-10-05 12:34:55 -0700

    Add files via upload

--- Project_expressions_alert/JS/main.js
+++ Project_expressions_alert/JS/main.js
@@ -1,9 +1,15 @@
 
 
-var a = 1, b=2, c=3, d=5;
-var a = a.fontcolor("red)") // turns the font red
-var b = b.fontcolor("blue");
-var c = c.fontcolor("green");
-
-
+var a = "The variable's called \"a\"" + " This string is concatenated "; // stating the variable and also writing it while concatenating//
 document.write(a);
+<br></br>
+
+var family = "the house", dad = "branden", mom= "Li", daughter ="Quark"  // declare all variables and give them a string value//
+var family =family.fontcolor("blue")
+var dad = dad.fontcolor("red") //now we assign colors to them all and write one//
+var mom = mom.fontcolor("blue")
+var daughter = daughter.fontcolor("yellow");
+
+document.write(daughter);
+
+document.write(2-1);  // a simple write statement//
